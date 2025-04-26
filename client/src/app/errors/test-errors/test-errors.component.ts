81fe05c15dee1e380461f991e04534be34c1ee41
jinsjob <39972022+jinsjob@users.noreply.github.com>
2025-04-26 19:00:58 +1200

    End of section 7

---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { response } from 'express';
+
+@Component({
+  selector: 'app-test-errors',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-errors.component.html',
+  styleUrl: './test-errors.component.css'
+})
+export class TestErrorsComponent {
+  baseUrl = 'https://localhost:5001/api/';
+  private httpClient = inject(HttpClient);
+  validationErrors: string[] = [];
+
+  get400Error() {
+    this.httpClient.get(this.baseUrl + 'error/bad-request').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get401Error() {
+    this.httpClient.get(this.baseUrl + 'error/auth').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get404Error() {
+    this.httpClient.get(this.baseUrl + 'error/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get500Error() {
+    this.httpClient.get(this.baseUrl + 'error/server-error').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400ValidationError() {
+    this.httpClient.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: response => console.log(response),
+      error: error => {
+        console.log(error)
+        this.validationErrors = error;
+      }
+    })
+  }
+}
