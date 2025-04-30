c9c3411578280a0cc33964985288f1a5674b3e84
jinsjob <39972022+jinsjob@users.noreply.github.com>
2025-04-30 17:39:47 +1200

    End of section 9

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { response } from 'express';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-test-errors',
@@ -10,7 +11,7 @@ import { response } from 'express';
   styleUrl: './test-errors.component.css'
 })
 export class TestErrorsComponent {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private httpClient = inject(HttpClient);
   validationErrors: string[] = [];
 
